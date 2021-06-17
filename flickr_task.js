8e6d9b0bb593821fff3b3a0e1328c40a4535d9a3
Alekseych4 <alekseych@list.ru>
2021-06-18 00:16:48 +0300

    flickr task was revised

--- flickr_task.js
+++ flickr_task.js
@@ -1,24 +1,34 @@
+var timeoutId;
+
 var slideshow = function(tag) {
-	// console.log(tag);
+
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + 
 			"tags=" + tag + "&format=json&jsoncallback=?";
+
 	var displayMessage = function (messageIndex) {
-		if (tag !== "")  {
-			$.getJSON(url, function (flickrResponse) {
-				console.log(messageIndex);
-				var $img = $("<img>").attr("src", flickrResponse.items[messageIndex].media.m).hide();
+
+		var changeImg = function (items, listIndex) {
+			if (listIndex < items.length) {
+				var $img = $("<img>").attr("src", items[listIndex].media.m).hide();
 				$("main .photos").empty();
 				$("main .photos").append($img);
 				$img.fadeIn();
-				setTimeout(function () {
-					messageIndex = messageIndex + 1;
-					displayMessage(messageIndex);
-				}, 3000);
-				if (messageIndex === 2) {
-						messageIndex = -1;
-				}
-			});
+				listIndex += 1;
+			} else{
+				listIndex = 0;
+			}
+
+			timeoutId = setTimeout(function(){
+				changeImg(items, listIndex);
+			}, 3000);
 		}
+		
+		$.getJSON(url, function (flickrResponse) {
+			console.log(messageIndex);
+			
+			changeImg(flickrResponse.items, 0);
+		});
+		
 	};
 	displayMessage(0);
 }
@@ -29,12 +39,14 @@ var main = function () {
 	var $inputLabel = $("<p>").text("Введите тег для темы слайд-шоу: "),
 		$input = $("<input>").addClass("tag"), 				
 		$button = $("<button>").text("Поиск");
+
 	$button.on("click", function () {
 		var tag = "";
 		tag = $input.val();
-		// console.log(tag);
+		
 		$input.val("");
 		if (tag !== "") {
+			clearTimeout(timeoutId);
 			$("main .photos").empty();
 			slideshow(tag);
 		}
